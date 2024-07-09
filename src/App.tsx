2e9e47173bcfe14894b72d54f46d588eca796caa
maci-kb24 <macikb8@yahoo.com>
2024-07-09 15:46:18 +0200

    create taskcard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
+import { useState } from 'react'
 import TaskCard from './components/TaskCard'
+import {  statuses, Task } from './utils/data-tasks'
 
 function App() {
+  const [tasks,setTasks] = useState<Task[]>([]) 
+  const columns = statuses.map(status => {
+    const TaskInColumn = tasks.filter(task => task.status === status)
+    return {
+      status,
+      tasks: TaskInColumn
+    }
+  })
 
   return (
-    <>
-      <TaskCard />
-    </>
+    <div className='flex divide-x'>
+      {columns.map(column => (
+        <div>
+          <h1>{column.title}</h1>
+          {column.tasks.map(task => <TaskCard task={task} />)}
+        </div>
+      ))}
+    </div>
   )
 }
 
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,7 +1,32 @@
+import { Task } from '../utils/data-tasks'
 
-const TaskCard = () => {
+const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
+</svg>
+const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
+</svg>
+const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+</svg>
+
+const TaskCard = ({ task }: 
+  { task: Task}) => {
   return (
-    <div>TaskCard</div>
+    <div className='border rounded-lg px-2 m-2 bg-gray w-56'>
+      <div className='text-base font-semibold py-2'>
+        {task.title}!!!
+      </div>
+      <div className='text-sm'>
+        <div>{task.id}</div>
+        <div>{task.priority}</div>
+        {task.priority === 'high' && highPriorityIcon}
+        {task.priority === 'medium' && mediumPriorityIcon}
+        {task.priority === 'low' && lowPriorityIcon}
+        <div>{task.points}</div>
+      </div>
+    </div>
   )
 }
 
