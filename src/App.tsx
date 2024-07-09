e3473828ce3ce0f31df92f16c087661b4b447edd
maci-kb24 <macikb8@yahoo.com>
2024-07-09 15:25:18 +0200

    create db.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
+import TaskCard from './components/TaskCard'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+      <TaskCard />
     </>
   )
 }
